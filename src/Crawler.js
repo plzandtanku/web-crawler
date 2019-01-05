0b08f961a5dc539ddec95a29a4871535c6c1d623
plzandtanku <mrkennethliu@gmail.com>
2019-01-04 16:03:58 -0800

    Added wait msg

--- src/Crawler.js
+++ src/Crawler.js
@@ -8,7 +8,7 @@ class Crawler extends Component {
     this.state = {
       query: '',
       startUrl: '',
-			result: '',
+      result: '',
     };
 		
     this.handleChange = this.handleChange.bind(this);
@@ -24,56 +24,59 @@ class Crawler extends Component {
   handleSubmit(event) {
     event.preventDefault();
 	
-		if (!this.state.startUrl) {
+    if (!this.state.startUrl) {
 	//		this.setState({
 		//		startUrl : "https://en.wikipedia.org/wiki/SpongeBob_SquarePants"
 			//	});
 			this.urls=["https://en.wikipedia.org/wiki/SpongeBob_SquarePants"];
 		}
 	else {
-	this.urls = [this.state.startUrl];
+		this.urls = [this.state.startUrl];
 	}
 
-		console.log(this.urls);
-		this.search(this.state.query);
+	console.log(this.urls);
+	this.setState({
+		result: "Running query, plz wait...",
+	});
+	this.search(this.state.query);
   }
-	search(q) {
-		if (this.urls.length < 1) return;
-		let bypass = "https://cors-anywhere.herokuapp.com/";
-		let re = new RegExp(q);
-		let url = this.urls[0];
-		//https://stackoverflow.com/questions/24710989/how-do-i-make-http-requests-inside-a-loop-in-nodejs
-		request(bypass+url, (err, res, body) => {
-			this.urls.shift();
-			//		console.log(url);
-			//	console.log(this.urls.length);
-			if (body.match(re)){
-				console.log("ya burnt");
-				console.log(url);
-				this.setState({
-					result : url
-				});
+  search(q) {
+  	if (this.urls.length < 1) return;
+  	let bypass = "https://cors-anywhere.herokuapp.com/";
+  	let re = new RegExp(q);
+  	let url = this.urls[0];
+	//https://stackoverflow.com/questions/24710989/how-do-i-make-http-requests-inside-a-loop-in-nodejs
+	request(bypass+url, (err, res, body) => {
+		this.urls.shift();
+		//		console.log(url);
+		//	console.log(this.urls.length);
+		if (body.match(re)){
+			console.log("ya burnt");
+			console.log(url);
+			this.setState({
+				result : url
+			});
 
-				this.urls = [];
-				return url;
-			} 
-			else {
-				let test = document.createElement('html');
-				test.innerHTML = body;
-				let x = Array.from(test.getElementsByTagName('a'));
-				//	for (let i=0; i < x.length;i++){
-				if (x !== undefined && x.length > 0) {
-					for (let i=0; i < x.length;i++){
-						if (x[i].hasAttribute("href")) {
-							let link = x[i].getAttribute("href");
-							this.urls.push(link);
-						}
+			this.urls = [];
+			return url;
+		} 
+		else {
+			let test = document.createElement('html');
+			test.innerHTML = body;
+			let x = Array.from(test.getElementsByTagName('a'));
+			//	for (let i=0; i < x.length;i++){
+			if (x !== undefined && x.length > 0) {
+				for (let i=0; i < x.length;i++){
+					if (x[i].hasAttribute("href")) {
+						let link = x[i].getAttribute("href");
+						this.urls.push(link);
 					}
 				}
 			}
-			this.search(q);
-		});
-	}
+		}
+		this.search(q);
+	});
+  }
 	
   render() {
     return (
